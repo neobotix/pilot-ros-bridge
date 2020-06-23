09caa81eb648e9842c35ed9bc4ab88acae62b4ac
Max Wittal <wittal@neobotix.de>
2020-06-23 16:59:52 +0200

    compiling with new pilot-core install

--- codegen.sh
+++ codegen.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/usr/interface}
+VNX_INTERFACE_DIR=${VNX_INTERFACE_DIR:-/opt/neobotix/pilot-core/interface}
 
 cd $(dirname "$0")
 
-vnxcppcodegen --cleanup generated/ pilot.ros modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/automy/math/ ${VNX_INTERFACE_DIR}/automy/basic/ ${VNX_INTERFACE_DIR}/pilot/ ${VNX_INTERFACE_DIR}/pilot/kinematics/ ${VNX_INTERFACE_DIR}/pilot/kinematics/differential/
+vnxcppcodegen --cleanup generated/ pilot.ros modules/ ${VNX_INTERFACE_DIR}/vnx/ ${VNX_INTERFACE_DIR}/automy/math/ ${VNX_INTERFACE_DIR}/automy/basic/ ${VNX_INTERFACE_DIR}/pilot/ ${VNX_INTERFACE_DIR}/pilot/base/ ${VNX_INTERFACE_DIR}/pilot/kinematics/ ${VNX_INTERFACE_DIR}/pilot/kinematics/differential/
 

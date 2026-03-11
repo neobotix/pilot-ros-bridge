830ae7d51e9755904b5666430f0839928b2f60f3
jaw <wenzel@neobotix.de>
2026-03-11 10:23:45 +0100

    handling multiple emergency signals

--- src/ROS_Bridge.cpp
+++ src/ROS_Bridge.cpp
@@ -298,14 +298,8 @@ void ROS_Bridge::handle(std::shared_ptr<const EmergencyState> value){
 	out->header.stamp = pilot_to_ros_time(value->time);
 
 	// assign input (laser, button) specific EM state
-	out->emergency_button_stop = false;
-	out->scanner_stop = false;
-
-	// Scanner stop or EMStop
-	switch (value->code) {
-		case safety_code_e::SCANNER_STOP: out->scanner_stop = true; break;
-		case safety_code_e::EMERGENCY_STOP: out->emergency_button_stop = true; break;
-	}
+	out->emergency_button_stop = (value->codes.find(safety_code_e::EMERGENCY_STOP) != value->codes.end());
+	out->scanner_stop = (value->codes.find(safety_code_e::SCANNER_STOP) != value->codes.end());
 
 	// State of the EMStop
 	switch (value->state) {
